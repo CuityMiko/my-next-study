10aad6aa27c8dece810aed8e944fc3afb5fdbbda
cuitongyang <shundaibei_cuity@git.cloud.tencent.com>
2019-06-13 06:47:48 +0800

    启动next

--- server/index.js
+++ server/index.js
@@ -1,3 +1,22 @@
-var koa = require('koa');
-var next = require('next');
+const koa = require('koa');
+const next = require('next');
+
+// 判断是否是生产环境
+const dev = process.env.NODE_ENV != 'production'
+const app = next({dev});
+
+const handle = app.getRequestHandler();
+// 页面预处理完成后
+app.prepare().then(() => {
+    const server = new koa();
+
+    server.use(async (ctx, next) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+    })
+
+    server.listen(3001, () => {
+        console.log(`next koa server listen on 3001!`)
+    })
+})
 
